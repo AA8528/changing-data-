8be1bd1dbe599e760423824f4e21c2d0e71b2f36
Adarsh <91724820+AA8528@users.noreply.github.com>
2022-05-29 16:57:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let data=["adarsh",15,"pizza"]
+
+function firstFunction(){
+    alert("My name is: "+data[0]+" age: "+data[1]+" food: "+data[2])
+}
+
+firstFunction()
+
+data=["shri",19,"burger"]
+firstFunction()
